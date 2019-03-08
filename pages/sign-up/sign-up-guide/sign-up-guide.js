0fab9046ef4d98a05dcff23e5f093ba6f7016404
unknown <unknown@example.com>
2019-03-08 16:01:51 +0800

    简单优化

--- pages/sign-up/sign-up-guide/sign-up-guide.js
+++ pages/sign-up/sign-up-guide/sign-up-guide.js
@@ -36,79 +36,78 @@ Page({
     let that = this;
     wx.chooseImage({
       count: 1,
-      success: function (e) {
+      success: function (imageInfo) {
         that.setData({
-          imageSrc: e.tempFilePaths[0]
+          imageSrc: imageInfo.tempFilePaths[0]
         })
         let fs = wx.getFileSystemManager();
         fs.readFile({
-          filePath: e.tempFilePaths[0],
+          filePath: imageInfo.tempFilePaths[0],
           encoding: 'base64',
-          success: res => {
-            let orientatioObject = {
-              side : orientation
-            }
-            wx.showLoading({
-              title: '加载中',
-            })
-            wx.request({
-              url: 'https://dm-51.data.aliyun.com/rest/160601/ocr/ocr_idcard.json',
-              data: {
-                'image': res.data,
-                "configure": orientatioObject
-              },
-              header: {
-                "Authorization": "APPCODE 8203b67bae954fa68ab249744beebbf5"
-              },
-              method: 'POST',
-              dataType: 'json',
-              responseType: 'text',
-              success: function (res) {
-                console.log(res.data);
-                if (200 == res.statusCode) {
-                  if(orientation == "face"){
-                    app.globalData.faceData = res.data;
-                    that.setData({
-                      faceImg: e.tempFilePaths[0]
-                    })
-                  }else{
-                    app.globalData.backData = res.data;
-                    that.setData({
-                    backImg: e.tempFilePaths[0]
-                    })
-                  }
-                  if (app.globalData.faceData && app.globalData.backData){
-                    that.setData({
-                      buttonDisable: false
-                    })
-                  }
-                  console.log(app.globalData);
-                } else {
-                  wx.showModal({
-                    title: '提示信息',
-                    content: '你选择的图片不符合规格，请重新上传',
-                    showCancel: false,
-                    confirmText: '确定',
-                    success: function (res) {
-                      console.log("点击了确定", res);
-                    },
-                  })
-                }
-              },
-              fail: function (res) {
-                console.log('fail');
-              },
-              complete:function(res){
-                wx.hideLoading();
-              }
-            })
+          success: fileInfo => {
+            that.requestOcr(fileInfo, orientation, imageInfo);
           },
-        });
-      }
+        })
+      },
     })
   },
-
-  // requestOcr: function () {
-
-  // } 
+  requestOcr: function (fileInfo, orientation, imageInfo) {
+    let that = this;
+    let orientatioObject = {
+      side: orientation
+    }
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.request({
+      url: 'https://dm-51.data.aliyun.com/rest/160601/ocr/ocr_idcard.json',
+      data: {
+        'image': fileInfo.data,
+        "configure": orientatioObject
+      },
+      header: {
+        "Authorization": "APPCODE 8203b67bae954fa68ab249744beebbf5"
+      },
+      method: 'POST',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        console.log(res);
+        if (200 === res.statusCode) {
+          if (orientation == "face") {
+            app.globalData.faceData = res.data;
+            that.setData({
+              faceImg: imageInfo.tempFilePaths[0]
+            })
+          } else {
+            app.globalData.backData = res.data;
+            that.setData({
+              backImg: imageInfo.tempFilePaths[0]
+            })
+          }
+          if (app.globalData.faceData && app.globalData.backData) {
+            that.setData({
+              buttonDisable: false
+            })
+          }
+          console.log(app.globalData);
+        } else {
+          wx.showModal({
+            title: '提示信息',
+            content: '你选择的图片不符合规格，请重新上传',
+            showCancel: false,
+            confirmText: '确定',
+            success: function (res) {
+              console.log("点击了确定", res);
+            },
+          })
+        }
+      },
+      fail: function (res) {
+      },
+      complete: function (res) {
+        wx.hideLoading();
+      },
+    })
+  }
 })
