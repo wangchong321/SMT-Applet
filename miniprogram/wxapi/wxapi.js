e1165b0224384e626ef9ae591785197b35af4f57
wangchong321 <912321420@qq.com>
2019-12-18 10:06:00 +0800

    小修改

--- miniprogram/wxapi/wxapi.js
+++ miniprogram/wxapi/wxapi.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = 'https://mobile-ms.uat.homecreditcfc.cn/mock/5caab2b66696420027621bfc/TAPP'
+const POST = 'post'
 
 const request = (url, method, data) => {
   let _url = API_BASE_URL + url;
@@ -41,50 +42,50 @@ module.exports = {
   request,
   // 客户状态->客户列表
   customers: (data) => {
-    return request('/api/tipper/payment/notaccepted','post',data)
+    return request('/api/tipper/payment/notaccepted', POST,data)
   },
   // 客户状态->访问数量
   scancount:(data) => {
-    return request('/api/tipper/payment/scancount', 'post', data)
+    return request('/api/tipper/payment/scancount', POST, data)
   },
   // 基本信息
   baseInfo:(data) => {
-    return request('/api/tipper/tipper-info', 'post', data)
+    return request('/api/tipper/tipper-info', POST, data)
   },
   // 居间服务费->总额
   paymentSum: (data) => {
-    return request('/api/tipper/payment/paymentsum', 'post', data)
+    return request('/api/tipper/payment/paymentsum', POST, data)
   },
   // 居间服务费->详情列表
   paymentList: (data) => {
-    return request('/api/tipper/payment/paymentlist', 'post', data)
+    return request('/api/tipper/payment/paymentlist', POST, data)
   },
   //门店->可分配的门店总数据
   getSelectableStores: (data) => {
-    return request('/api/tipper-dsm/get-pos-view-data','post',data)
+    return request('/api/tipper-dsm/get-pos-view-data', POST,data)
   },
   //上传注册信息->选择完门店后，把数据上传
   uploadTipperInfo: (data) => {
-    return request('/api/tipper/update-tipper-ocr', 'post', data)
+    return request('/api/tipper/update-tipper-ocr', POST, data)
   },
   //验证Homer id->输入Homer id后第一次验证
   tipperRegisterValidateHid: (data) => {
-    return request('/api/register/validate-hid','post',data)
+    return request('/api/register/validate-hid', POST,data)
   },
   //发送验证码->点击发送按钮，发送短信验证码
   tipperRegisterSendVcode: (data) => {
-    return request('/api/register/send-vcode-tipper-by-mobile', 'post', data)
+    return request('/api/register/send-vcode-tipper-by-mobile', POST, data)
   },
   //验证账号->验证homer id和sms_code后登陆
   tipperRegisterValidateVcode: (data) => {
-    return request('/api/register/validate-vcode', 'post', data)
+    return request('/api/register/validate-vcode', POST, data)
   },
   //上传tipper信息注册登陆
   tipperRegisterUpateInfo: (data) => {
-    return request('/api/tipper/register', 'post', data)
+    return request('/api/tipper/register', POST, data)
   },
   //上传tipper信息注册登陆
   tipperPaymentHistory: (data) => {
-    return request('/api/tipper/payment/paymenthistory', 'post', data)
+    return request('/api/tipper/payment/paymenthistory', POST, data)
   }
 }
